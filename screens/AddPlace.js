f730e8ce10dbbd80c1f195b818f01e8b6fae4695
Sinan Demirbilek <63739956+sinandemirbilek@users.noreply.github.com>
2023-11-27 10:47:48 +0300

    initial commit

---
+++ screens/AddPlace.js
@@ -0,0 +1,15 @@
+import PlaceForm from "../components/Places/PLaceForm";
+import { insertPlace } from "../util/database";
+
+
+
+function AddPlace({ navigation }) {
+  async function createPlaceHandler(place) {
+    await insertPlace(place);
+    navigation.navigate('AllPlaces');
+  }
+
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
+}
+
+export default AddPlace;
